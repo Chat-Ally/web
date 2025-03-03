7ac2f4465381a109b8c76b8cff85a84859d8e879
Daniel <daanielescobar@gmail.com>
2025-03-03 18:12:52 +0000

    add white background

--- components/landing/meteors-card.tsx
+++ components/landing/meteors-card.tsx
@@ -12,7 +12,7 @@ export function MeteorsCard(props: props) {
     return (
         <div className=" w-full h-full relative mx-auto max-w-xs ">
             <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.95] bg-red-500 rounded-md blur-lg " />
-            <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full w-full overflow-hidden rounded-2xl flex flex-col justify-center items-center">
+            <div className="relative shadow-xl bg-white dark:bg-gray-900 border border-gray-800  px-4 py-8 h-full w-full overflow-hidden rounded-2xl flex flex-col justify-center items-center">
 
                 <div className="flex flex-col justify-center items-center text-base text-center text-slate-500 relative z-50">
                     {props.icon}
