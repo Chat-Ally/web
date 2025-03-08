0827f28a4c118a3de1888ba5a826dec8f32632a6
Daniel <daanielescobar@gmail.com>
2025-03-08 20:30:11 +0000

    proof that api endpoints can be protected with supabase session

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import createClient from "@/lib/supabase/api";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   name: string;
+  data: any
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: "John Doe" });
+  let supabase = createClient(req, res)
+  let { data, error } = await supabase.auth.getUser()
+
+  if (data) {
+    res.status(200).json({
+      name: "user logged in",
+      data: data
+    })
+  }
+
+  if (error) {
+    res.status(200).json({
+      name: "not logged in",
+      data: error
+    });
+  }
 }
