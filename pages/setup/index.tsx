3d61259c0a6527b34961335937361b37eeca3a44
Daniel <daanielescobar@gmail.com>
2025-03-16 23:16:56 +0000

    get qr code and update if it changes

--- pages/setup/index.tsx
+++ pages/setup/index.tsx
@@ -2,17 +2,39 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
-import Link from "next/link";
+import { createClient } from "@/lib/supabase/component";
+import { useRouter } from "next/router";
 
 export default function Setup() {
+
+    const router = useRouter()
+    const supabase = createClient()
 
     const [name, setName] = useState<string>("")
     const [businessName, setBusinessName] = useState<string>("")
     const [logo, setLogo] = useState()
 
+    async function saveBusinessData() {
+        const { data: user, error: userError } = await supabase.auth.getUser()
+
+        const { data, error } = await supabase
+            .from("business")
+            .insert([{
+                name: businessName,
+                owner_id: user.user?.id,
+            }])
+            .select()
+
+        if (error) {
+            console.error(error)
+        } else {
+            router.push('/setup/qr')
+        }
+    }
+
     return (
         <div className="w-full h-screen flex justify-center items-center align-middle">
-            <div className="bg-slate-50 rounded-md border p-8 md:p-16 mx-4">
+            <div className="bg-slate-50 dark:bg-neutral-900 rounded-md border border-neutral-700 p-8 md:p-16 mx-4">
                 <h1 className="font-bold text-2xl">Empecemos</h1>
                 <h2>Vamos a configurar algunos detalles de tu negocio</h2>
 
@@ -27,7 +49,6 @@ export default function Setup() {
                     />
                 </div>
 
-
                 <div className="mt-2">
                     <Label htmlFor="business_name">Nombre de tu negocio</Label>
                     <Input
@@ -38,7 +59,6 @@ export default function Setup() {
                         onChange={(e) => setBusinessName(e.target.value)}
                     />
                 </div>
-
 
                 <div className="mt-2">
                     <Label />
@@ -51,9 +71,12 @@ export default function Setup() {
                     />
                 </div>
 
-                <Link href={'/setup/qr'}>
-                    <Button className="mt-2">Guardar y continuar</Button>
-                </Link>
+                <Button
+                    onClick={saveBusinessData}
+                    className="mt-2"
+                >
+                    Guardar y continuar
+                </Button>
             </div>
         </div>
     )
--- pages/setup/qr.tsx
+++ pages/setup/qr.tsx
@@ -1,19 +1,77 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { createClient } from '@/lib/supabase/component';
 import Link from 'next/link';
 import { QRCodeCanvas } from 'qrcode.react';
-
+import { useEffect, useState } from 'react';
 
 export default function QR() {
+    const supabase = createClient()
+    const [qrCode, setQRCode] = useState()
+
+    useEffect(() => {
+        async function getQR() {
+            let { data, error } = await supabase
+                .from('whatsapp-containers')
+                .select('*')
+                .eq('id', 1) // you already know we have to change this
+                .single()
+            if (error) console.error(error)
+            if (data) {
+                setQRCode(data.qr)
+            }
+        }
+        getQR()
+    }, [])
+
+    useEffect(() => {
+
+        const channel = supabase.channel('qr')
+            .on(
+                'postgres_changes',
+                {
+                    event: '*',
+                    schema: 'public',
+                    table: 'whatsapp-containers'// add filters later
+                },
+                (payload: any) => {
+                    setQRCode(payload.new.qr)
+                }
+            )
+            .subscribe()
+
+        return () => {
+            supabase.removeChannel(channel)
+        }
+    }, [supabase])
+
     return (
-        <div className="w-full h-screen flex justify-center items-center align-middle">
-            <div className="bg-slate-50 rounded-md border p-8 md:p-16 mx-4">
-                <h1 className="font-bold text-2xl">Empecemos</h1>
-                <h2>En Whatsapp, dirigete a "Vincular dispositivo" y escanea este codigo.</h2>
-                <QRCodeCanvas className='mx-auto mt-12' size={256} value={"https://hola.com"} />
-                <Link href="/dashboard">
-                    <Button className='mt-12'>Finalizar</Button>
-                </Link>
-            </div>
+        <div className="h-screen flex flex-col justify-center ">
+            <Card className='mx-auto max-w-md dark:bg-neutral-900'>
+                <CardHeader>
+                    <CardTitle >Empecemos</CardTitle>
+                    <CardDescription>En tu teléfono, abre Whatsapp, dirigete a "Vincular dispositivo" y escanea este código para poder automatizar tu cuenta.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    {
+                        qrCode ?
+                            <QRCodeCanvas
+                                className='mx-auto mt-4'
+                                size={256}
+                                value={qrCode}
+                            />
+                            :
+                            <>loading</>
+                    }
+                </CardContent>
+                <CardFooter>
+                    <Link href="/dashboard">
+                        <Button className='mt-4'>Finalizar</Button>
+                    </Link>
+                </CardFooter>
+            </Card>
         </div>
     )
 }
