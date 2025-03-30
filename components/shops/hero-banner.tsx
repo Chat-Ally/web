33a5b9090fbd2b6d1014888482f1a84b9f695b88
Daniel <daanielescobar@gmail.com>
2025-03-30 00:07:05 +0000

    make banner smaller

--- components/shops/hero-banner.tsx
+++ components/shops/hero-banner.tsx
@@ -1,13 +1,10 @@
-import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
-import { Button } from "../ui/button";
-
 export default function HeroBanner() {
     return (
-        <div className="relative w-full h-[600px] overflow-hidden">
+        <div className="relative w-full h-[450px] overflow-hidden">
             <div className="absolute inset-0 bg-gradient-to-b from-black/30 to-transparent z-10" />
             <img
                 src='https://fxyribxpczjutxfikcuw.supabase.co/storage/v1/object/public/landing//Galaxy%20Tab%20S8%20Ultra.png'
-                className={`absolute top-0 left-0 w-full h-full object-cover transition-all duration-1000 ease-in-out`}
+                className="absolute top-0 left-0 w-full h-full object-cover transition-all duration-1000 ease-in-out"
             />
         </div>
     )
